f03f96bf0b85c6d6b9858148fa5c78eba5a9de6d
williamdjones <derek.jones4@uky.edu>
2017-01-19 19:55:51 -0500

    Initial commit

---
+++ test.py
@@ -0,0 +1,43 @@
+import numpy as np
+import os
+import dicom
+import matplotlib.pyplot as plt
+
+
+plan = dicom.read_file('stage1/0/0_0.dcm')
+
+
+PathDicom = "./stage1/0/"
+lstFilesDCM = []  # create an empty list
+for dirName, subdirList, fileList in os.walk(PathDicom):
+    for filename in fileList:
+        if ".dcm" in filename.lower():  # check whether the file's DICOM
+            lstFilesDCM.append(os.path.join(dirName,filename))
+
+
+num_images = len(lstFilesDCM)
+
+image = plan.pixel_array
+
+xshape, yshape = image.shape
+
+
+image_data = np.ndarray([num_images,xshape,yshape])
+
+
+i = 0
+
+for dcmfile in lstFilesDCM:
+	plan = dicom.read_file(dcmfile)
+	 
+	image_data[i,:,:] = plan.pixel_array
+
+	i = i + 1
+
+
+#num_i = 5
+
+#for j in range(0,num_i):
+#	plt.imshow(image_data[j,:,:])
+#	plt.show()
+#	plt.close()
---
+++ train.py
@@ -0,0 +1,41 @@
+import numpy as np
+import pandas as pd
+
+
+
+from keras.layers import Dense, Convolution2D
+from keras.models import Sequential
+from keras.layers.advanced_activations import PReLU
+from utils import load_data
+
+
+
+data = load_data('./stage/0/0_0.dcm')
+
+labels = pd.read_csv('stage1_labels.csv')
+
+num_examples = data.shape[0]
+
+data = np.reshape(data,[num_examples,512,512,1])
+
+
+print data.shape
+
+print labels.shape
+
+print
+
+#model = Sequential()
+
+#model.add(Convolution2D(80,57,6,input_shape=(512,512,1),activation=PReLU(init='zero')))
+#model.add(Dense(10,activation='relu'))
+
+#model.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['accuracy'])
+
+#model.fit(data,labels,batch_size = 10, nb_epoch=100,verbose=1,shuffle=True)
+
+#scores = model.evaluate(data,labels,batch_size=10)
+
+#print scores[0]
+
+#print scores[1]
